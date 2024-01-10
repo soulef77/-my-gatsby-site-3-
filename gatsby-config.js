1b53b377328725823c9f445c863f9447d525260c
saoud <saoud.soulef@laposte.net>
2024-01-10 22:20:37 +0100

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,6 +60,17 @@ module.exports = {
           },
         },
       },
-    ],
+    ]
   }
+    
+    module.exports = {
+      siteMetadata: {
+        title: "My First Gatsby Site",
+      },
+      plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+      ],
+    };
+  
 
